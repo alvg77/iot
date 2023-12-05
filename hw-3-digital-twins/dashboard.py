68b8d6c5589944f1f0c8d076ac58fa3ef2bed859
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-12-06 01:31:05 +0200

    updated dashboard

--- hw-3-digital-twins/dashboard.py
+++ hw-3-digital-twins/dashboard.py
@@ -4,23 +4,23 @@ import time
 
 if __name__ == "__main__":
     while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
         # in case new bulbs are added
         filenames = os.listdir("./digital-twins")
-        bulb_data = []
-
-        for filename in filenames:
-            with open("./digital-twins/" + filename, 'r') as f:
-                bulb_data.append(json.load(f))
-
-        os.system('cls' if os.name == 'nt' else 'clear')
 
         print("Bulb ID | Color | Brightness | State")
-        for i in bulb_data:
-            print(
-                f"{i['thing_id']} | \
-                {i['features']['indicators']['properties']['color']} | \
-                {i['features']['indicators']['properties']['brightness']} | \
-                {i['features']['state']['properties']['on']} \
-            ")
+        print("------------------------------------")
+        for filename in filenames:
+            with open("./digital-twins/" + filename, 'r') as f:
+                try:
+                    data = json.load(f)
+                    print(
+                        f"{data['thing_id']} | \
+                        {data['features']['indicators']['properties']['color']} | \
+                        {data['features']['indicators']['properties']['brightness']} | \
+                        {data['features']['state']['properties']['on']} \
+                    ")
+                except json.decoder.JSONDecodeError as e:
+                    print("Error loading file: " + filename)
         
         time.sleep(1)
